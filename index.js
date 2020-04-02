c398717a27b117932b271834a7d8bd6b9057b772
osbornechan <osbornecab@gmail.com>
2020-04-02 23:43:21 +0800

    Color output (does not work)

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const hexConvertToHsl = require('./hex-to-hsl.js');
 const rgbConvertToHex = require('./rgb-to-hex.js');
 const rgbConvertToHsl = require('./rgb-to-hsl.js');
 
+// set as red
+var r = 255;
+var b = 0;
+var g = 0;
+
+// this special console will create colored output
+//console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, myOutput);
+
 let hex = hexConvertToRgb.hexToRgb(process.argv[3])
 
 if (process.argv[2] == 'hex') {
-    console.log('rgb: ' + hex.r + ',' + hex.g + ',' + hex.b + ' , ' + hexConvertToHsl.hexToHSL(process.argv[3]));
+    console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`,
+        'rgb: ' + hex.r + ',' + hex.g + ',' + hex.b + ' , ' + hexConvertToHsl.hexToHSL(process.argv[3]));
 } else if (process.argv[2] == 'rgb') {
-    console.log('hex: ' + rgbConvertToHex.rgbToHex(process.argv[3], process.argv[4], process.argv[5]) + ' , ' + 'hsl(' +rgbConvertToHsl.rgbToHsl(process.argv[3], process.argv[4], process.argv[5]) + ')');
+    console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`,
+        'hex: ' + rgbConvertToHex.rgbToHex(process.argv[3], process.argv[4], process.argv[5]) + ' , ' + 'hsl(' +rgbConvertToHsl.rgbToHsl(process.argv[3], process.argv[4], process.argv[5]) + ')');
 }
